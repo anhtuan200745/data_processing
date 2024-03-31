530655c3b97e3f7d2345b32de9a360f708d3ebed
anhtuan200745 <tuannvats2208021@fpt.edu.vn>
2024-04-01 01:17:09 +0700

    final update

--- dataset_processing/processing/rewrite_image_path.py
+++ dataset_processing/processing/rewrite_image_path.py
@@ -1,5 +1,9 @@
 import os
 import csv
+import pydicom
+import shutil
+
+csv_path = "mass_case_description_test_set_mini.csv"
 
 
 def extract_patient_id(file_path):
@@ -17,50 +21,80 @@ def find_pathology(patient_id, read_csv_path):
 
     return None
 
-def rewrite(root_folder, read_csv_path, write_csv_path):
+
+def check_masked_image(file_path):
+    masked = False
+    file_data = pydicom.dcmread(file_path)
+
+    pixel_data = file_data.pixel_array
+
+    max_pixel_value = pixel_data.max()
+    min_pixel_value = pixel_data.min()
+
+    if max_pixel_value == 255 and min_pixel_value == 0:
+        masked = True
+        return masked
+    else:
+        return masked
+
+
+def move_and_rename_image(image_path, folder_path):
+    patient_id = extract_patient_id(image_path)
+    pathology = find_pathology(patient_id, csv_path)
+
+    if "test" in image_path.lower():
+        subfolder = "test"
+    elif "training" in image_path.lower():
+        subfolder = "training"
+    else:
+        subfolder = ""
+
+    subfolder_path = os.path.join(folder_path, subfolder)
+    pathology_path = os.path.join(subfolder_path, pathology)
+    if not os.path.exists(pathology_path):
+        os.makedirs(pathology_path)
+
+    if "_1" in image_path:
+        if check_masked_image(image_path):
+            image_name_base = patient_id + "_masked_image"
+        else:
+            image_name_base = patient_id + "_cropped_image"
+    else:
+        image_name_base = patient_id + "_image"
+
+    existing_files = [file for file in os.listdir(pathology_path) if image_name_base in file]
+
+    count = len(existing_files)
+
+    if count > 0:
+        image_name = f"{image_name_base}_{count + 1}{os.path.splitext(image_path)[1]}"
+    else:
+        image_name = f"{image_name_base}{os.path.splitext(image_path)[1]}"
+    new_image_path = os.path.join(pathology_path, image_name)
+
+    shutil.move(image_path, new_image_path)
+
+    print(f"Image moved and renamed to {new_image_path}")
+
+
+def rewrite(root_folder):
     if not os.path.exists(root_folder):
         print("Root folder not found.")
         return
-
-    image_paths = {}
-    cropped_paths = {}
-    masked_paths = {}
-    patient_ids_seen = set()
-    file_paths_seen = set()
-
+    moved_file = 0
     for root, dirs, files in os.walk(root_folder):
         for file in files:
             if file.lower().endswith('.dcm'):
                 file_path = os.path.join(root, file)
-                if 'Mass-Test' in file_path:
-                    if '_1' in file_path:
-                        if '1-1.dcm' in file:
-                            masked_paths[file_path] = file_path
-                        elif '1-2.dcm' in file:
-                            cropped_paths[file_path] = file_path
-                    else:
-                        patient_id = extract_patient_id(file_path)
-                        if patient_id not in patient_ids_seen and file_path not in file_paths_seen:
-                            image_paths[file_path] = file_path
-                            patient_ids_seen.add(patient_id)
-                            file_paths_seen.add(file_path)
-
-    with open(write_csv_path, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        writer.writerow(["patient_id", "image_path", "cropped_path", "masked_path", "pathology"])
-        for image_path, cropped_path, masked_path in zip(image_paths.values(), cropped_paths.values(),
-                                                         masked_paths.values()):
-            patient_id = extract_patient_id(image_path)
-            print(patient_id)
-            pathology = find_pathology(patient_id, read_csv_path)
-            print(pathology)
-            writer.writerow([patient_id, image_path, cropped_path, masked_path, pathology])
-
-    print("New CSV file created successfully.")
+                move_and_rename_image(file_path, "D:\\Project\\dataset_processing\\BreastCancer")
+                moved_file += moved_file
+    return moved_file
 
 
+def main():
+    root_folder = "D:/Project/dataset_processing/CBIS-DDSM"
+    print(rewrite(root_folder))
 
-root_folder = "D:/Project/dataset_processing/CBIS-DDSM"
-write_csv_path = "data_result.csv"
-read_csv_path = "mass_case_description_test_set_mini.csv"
-rewrite(root_folder,read_csv_path, write_csv_path)
+
+if __name__ == "__main__":
+    main()
